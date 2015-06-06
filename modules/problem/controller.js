c4ac4a1ca203a2999de4ba1c40e158857381be4c
lmy <lmysoar@hotmail.com>
2015-06-06 16:44:41 +0800

    problem有了accepted属性，在展示所有的题目的时候

--- modules/problem/controller.js
+++ modules/problem/controller.js
@@ -11,15 +11,17 @@
   INDEX_PAGE = '.';
 
   exports.getIndex = function(req, res) {
-    var Group, User, currentProblems;
+    var Group, User, currentProblems, currentUser;
     Group = global.db.models.group;
     User = global.db.models.user;
     currentProblems = void 0;
+    currentUser = void 0;
     return global.db.Promise.resolve().then(function() {
       if (req.session.user) {
         return User.find(req.session.user.id);
       }
     }).then(function(user) {
+      currentUser = user;
       return myUtils.findProblems(user, [
         {
           model: Group
@@ -55,6 +57,21 @@
         p.triedPeopleCount = 0;
         if (tmp[p.id]) {
           p.triedPeopleCount = tmp[p.id];
+        }
+      }
+      return myUtils.getResultCount(currentUser, currentProblems, 'AC');
+    }).then(function(counts) {
+      var i, j, len, len1, p, tmp;
+      tmp = {};
+      for (i = 0, len = counts.length; i < len; i++) {
+        p = counts[i];
+        tmp[p.problem_id] = p.count;
+      }
+      for (j = 0, len1 = currentProblems.length; j < len1; j++) {
+        p = currentProblems[j];
+        p.accepted = 0;
+        if (tmp[p.id]) {
+          p.accepted = tmp[p.id];
         }
       }
       return currentProblems;
