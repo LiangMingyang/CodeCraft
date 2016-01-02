4e3f7b4a1e425f0286249892355e6a91520b86b8
lmy <lmysoar@hotmail.com>
2016-01-02 12:13:47 +0800

    init

---
+++ public/west/app.coffee
@@ -0,0 +1,3 @@
+angular.module('west', [])
+.controller('main',($scope)->
+)
---
+++ public/west/main_files/693181.js
@@ -0,0 +1,4 @@
+var online=new Array();
+document.write("<script language=\"javascript\" src=\"http://webpresence.qq.com/getonline?Type=1&2974752118:\" charset=\"utf-8\" type=\"text/javascript\"></script>");
+document.write("<script language=\"javascript\" src=\"http://code2.54kefu.net/kefu/url.js\" charset=\"utf-8\" type=\"text/javascript\"></script>");
+document.write("<script language=\"javascript\" src=\"http://code2.54kefu.net/kefu/js/181/693181_code.js\" charset=\"utf-8\" type=\"text/javascript\"></script>");
