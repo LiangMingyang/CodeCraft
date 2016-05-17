c14a2a882c264e9cb96ad5abaf38fb09aa879834
明阳 <lmysoar@hotmail.com>
2016-05-17 16:59:34 +0800

    减少了一个错误

--- modules/problem/controller.js
+++ modules/problem/controller.js
@@ -58,9 +58,10 @@
   };
 
   exports.postIndex = function(req, res) {
-    var Group, User, currentProblems, problemCount;
+    var Contest, Group, User, currentProblems, problemCount;
     Group = global.db.models.group;
     User = global.db.models.user;
+    Contest = global.db.models.contest;
     currentProblems = void 0;
     problemCount = void 0;
     return global.db.Promise.resolve().then(function() {
@@ -84,6 +85,9 @@
           model: User,
           attributes: ['id', 'nickname'],
           as: 'creator'
+        }, {
+          model: Contest,
+          attributes: ['id', 'title']
         }
       ]);
     }).then(function(result) {
